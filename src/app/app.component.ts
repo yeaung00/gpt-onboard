893240fa15b5f6eda8ee2981c8e39e32a1abb164
YeWint Aung <ye@YeWints-MBP.attlocal.net>
2023-10-09 16:28:33 -0700

    displays gpt api message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, filter, map } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { GptServiceService } from './gpt-service.service';
 
@@ -8,15 +8,18 @@ import { GptServiceService } from './gpt-service.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'gpt-test';
   result$: Subject<string> = new Subject<string>();
   prompt = new FormControl();
 
   getData() {
-    return this.gptService.getDataFromOpenAI(this.prompt.value).subscribe((data) => {
-      console.log(data)
-    })
+    this.gptService.getDataFromOpenAI(this.prompt.value)
+      .subscribe((data: any) => {
+        const output = data.choices[0].message.content
+        this.result$.next(output)
+      })
   }
   constructor(private gptService: GptServiceService) { }
 }
--- src/app/gpt-service.service.ts
+++ src/app/gpt-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from './../environments/environment';
+import { environment } from './../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class GptServiceService {
 
   constructor(private http: HttpClient) { }
 
-  getDataFromOpenAI(text: string) {
+  getDataFromOpenAI(text: string): any {
     const headers = new HttpHeaders().set('Authorization', 'Bearer ' + environment.OPENAI_KEY);
     const body = {
       'model': 'gpt-3.5-turbo',
@@ -17,6 +17,6 @@ export class GptServiceService {
       'messages': [{"role": "user", "content": text}],
       'max_tokens': 10,
     };
-    return this.http.post('https://api.openai.com/v1/chat/completions', body, { headers: headers });
+    return this.http.post('https://api.openai.com/v1/chat/completions', body, { headers: headers })
   }
 }
